d7b522fc2de0c5d2f908ef54f8ec7a14e37e3305
ilan <ilan19555@gmail.com>
2020-12-19 14:14:58 +0200

    Update NodeIn.js

--- NodeIn.js
+++ NodeIn.js
@@ -17,9 +17,9 @@ const client = new Client({
   ssl: process.env.DATABASE_URL ? true : false,
 });
 
-client
-  .connect()
-
+client.connect()
+.then(() => console.log("Connected"))
+.finally(()=>console.log("NOT"))
 client.query(
       "CREATE TABLE IF NOT EXISTS users (ID INT,Name VARCHAR(45),FamilyName VARCHAR(45),Email VARCHAR(45),PromoCode VARCHAR(45),Country VARCHAR(45) NULL,City VARCHAR(45) NULL,Street VARCHAR(45) NULL,ZipCode VARCHAR(45) NULL,Password VARCHAR(45) NULL,Spare1 VARCHAR(45) NULL,Spare2 VARCHAR(45) NULL,Spare3 INT NULL,Spare INT NULL)"
     )
