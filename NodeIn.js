9c43afacd7f9d863bcf50342e632d716f8961941
ilan <ilan19555@gmail.com>
2020-12-19 14:25:16 +0200

    Update NodeIn.js

--- NodeIn.js
+++ NodeIn.js
@@ -18,7 +18,7 @@ const client = new Client({
 });
 
 client.connect()
-.then(() => console.log("Connected"))
+.then(() => console.log("Connectedd"))
 .finally(()=>console.log("NOT"))
 client.query(
       "CREATE TABLE IF NOT EXISTS users (ID INT,Name VARCHAR(45),FamilyName VARCHAR(45),Email VARCHAR(45),PromoCode VARCHAR(45),Country VARCHAR(45) NULL,City VARCHAR(45) NULL,Street VARCHAR(45) NULL,ZipCode VARCHAR(45) NULL,Password VARCHAR(45) NULL,Spare1 VARCHAR(45) NULL,Spare2 VARCHAR(45) NULL,Spare3 INT NULL,Spare INT NULL)"
