0105e87a1dab7995965184300cbeb04b2aa9bd98
ilan <ilan19555@gmail.com>
2020-12-19 18:24:26 +0200

    a
    a

--- NodeIn.js
+++ NodeIn.js
@@ -110,7 +110,7 @@ app.post('/sign-up', async function (req, resul) {
     const client = await pool.connect();
   client.query(text,values,(err,res)=>{
   
-  if(res==undefined)
+  if(res.rows!=undefined)
   {
     console.log("GOT HERE")
     
