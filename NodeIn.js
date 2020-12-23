9e9b783be68d6a4d45ab834a6ac3d9dfe2549250
ilan <ilan19555@gmail.com>
2020-12-23 20:00:44 +0200

    Update NodeIn.js

--- NodeIn.js
+++ NodeIn.js
@@ -56,7 +56,7 @@ app.get('/db', async (req, resu) => {
       if(resi.rows.length==0)
         idNum=1;
         else
-        console.log(resi.rows)
+        console.log(resi.rows[0])
       idNum = resi.rows[1].id+1;
       
     })
