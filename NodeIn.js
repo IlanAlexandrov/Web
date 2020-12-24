0215560ed125532b42905059dda0a0a4a26c1905
ilan <ilan19555@gmail.com>
2020-12-24 10:02:50 +0200

    Update NodeIn.js

--- NodeIn.js
+++ NodeIn.js
@@ -23,18 +23,18 @@ const pool = new Pool({
 app.get('/db', async (req, resu) => {
   try {
     const client = await pool.connect();
-    await client.query('drop table users');
-    // const result1 = await client.query(
-    //   "CREATE TABLE IF NOT EXISTS users (ID INT,Name VARCHAR(45),FamilyName VARCHAR(45),Email VARCHAR(45),PhoneNumber VARCHAR(45),PromoCode VARCHAR(45),Country VARCHAR(45) NULL,City VARCHAR(45) NULL,Street VARCHAR(45) NULL,ZipCode VARCHAR(45) NULL,Password VARCHAR(45) NULL,Spare1 VARCHAR(45) NULL,Spare2 VARCHAR(45) NULL,Spare3 INT NULL,Spare INT NULL)"
-    // )
+    //await client.query('drop table users');
+    const result1 = await client.query(
+      "CREATE TABLE IF NOT EXISTS users (ID INT,Name VARCHAR(45),FamilyName VARCHAR(45),Email VARCHAR(45),PhoneNumber VARCHAR(45),PromoCode VARCHAR(45),Country VARCHAR(45) NULL,City VARCHAR(45) NULL,Street VARCHAR(45) NULL,ZipCode VARCHAR(45) NULL,Password VARCHAR(45) NULL,Spare1 VARCHAR(45) NULL,Spare2 VARCHAR(45) NULL,Spare3 INT NULL,Spare INT NULL)"
+    )
 
-    // await client.query("delete from users")
+    await client.query("delete from users")
 
-    // await client.query('SELECT * FROM users', (err, res) => {
-    //   console.log("HERE");
-    //   console.log(res.rows)
-    //   resu.redirect('/sign-up');
-    // })
+    await client.query('SELECT * FROM users', (err, res) => {
+      console.log("HERE");
+      console.log(res.rows)
+      resu.redirect('/sign-up');
+    })
 
     client.release();
   } catch (err) {
