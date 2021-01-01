85bcc0d60f0c4ed80156865a868283db82afd10e
ilan <ilan19555@gmail.com>
2021-01-01 09:36:08 +0200

    s
    s

--- NodeIn.js
+++ NodeIn.js
@@ -554,7 +554,7 @@ app.post('/remove', function (req, res) {
   res.clearCookie("Id");
   res.clearCookie("EmailU");
   res.clearCookie("FirstNAmeU")
-  res.sendFile(__dirname + "/LogIn.html");
+  res.send("Test")
 
 })
 app.get('/updateMail/:base64', async function (req, reso) {
